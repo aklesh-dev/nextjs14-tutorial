5484d9177e1d3eb40418be2729e0a90661cdb06b
aklesh-dev <arzunayavadu998@gmail.com>
2024-09-28 11:36:29 +0545

    feat: Image component
    - add image from external resourse
    - modified the next.config file to add external image
    - add imgs in public dir

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,12 @@
-
+import Image from "next/image";
+import styles from "./about.module.css";
 
 export default function AboutPage() {
   return (
-    <div>AboutPage</div>
+    <div>
+      <div className={styles.imgContainer}>
+        <Image src='https://images.pexels.com/photos/27957826/pexels-photo-27957826/free-photo-of-two-people-riding-motorcycles-down-a-road.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt="about" fill/>
+      </div>
+    </div>
   )
 };
