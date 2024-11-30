24c88307e2bfc6abff292dba3dc00417caf53af5
aklesh-dev <arzunayavadu998@gmail.com>
2024-11-30 13:05:48 +0545

    feat: Contact page design

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,26 @@
-
+import Image from 'next/image';
+import styles from './contact.module.css';
 
 export default function ContactPage() {
   return (
-    <div>contact page</div>
+    <section className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image          
+          src="/contact.png"
+          alt='Contact'    
+          fill      
+          className={styles.img}
+        />
+      </div>
+      <div className={styles.formContainer}>
+        <form className={styles.form}>
+          <input type="text" placeholder='Name and Surname' />
+          <input type="text" placeholder='Email Address' />
+          <input type="text" placeholder='Phone Number (optional)' />
+          <textarea name="" id="" cols="30" rows="10" placeholder='Message'></textarea>
+          <button type="submit">Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 };
